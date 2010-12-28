d328dfc441f7f76ab6896bddf4d0f7c7382e80a1
Andrew Mordecai <am@armord.net>
2010-12-28 13:32:58 +0000

    Updated tanking/healing checks Added Spanish localization

--- playerstate.lua
+++ playerstate.lua
@@ -2,6 +2,7 @@
 	oUF_Phanx
 	Fully-featured PVE-oriented layout for oUF.
 	by Phanx < addons@phanx.net >
+	Currently maintained by Akkorian < akkorian@hotmail.com >
 	Copyright © 2007–2010. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info13993-oUF_Phanx.html
 	http://wow.curseforge.com/addons/ouf-phanx/
@@ -14,72 +15,49 @@ if playerClass == "HUNTER" or playerClass == "MAGE" or playerClass == "ROGUE" or
 
 ns.eventFrame = CreateFrame("Frame")
 ns.eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-local TANKFORM
-
-local function tankCheck(_, event)
-	local form = GetShapeshiftForm() or 0
-	if form > 0 then
-		local _, name = GetShapeshiftFormInfo(form)
-		if name == TANKFORM then
-			ns.isTanking = true
-			return
-		end
-	end
-end
+ns.eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
 
 if playerClass == "DEATHKNIGHT" then
-	TANKFORM = GetSpellInfo(48263)
-	ns.eventFrame:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
-	ns.eventFrame:SetScript("OnEvent", tankCheck)
-	return
-end
 
-if playerClass == "DRUID" then
-	TANKFORM = GetSpellInfo(9634)
-	ns.eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
 	ns.eventFrame:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
 	ns.eventFrame:SetScript("OnEvent", function()
-		local t1, t2, t3 = select(3, GetTalentTabInfo(1)) or 0, select(3, GetTalentTabInfo(2)) or 0, select(3, GetTalentTabInfo(3)) or 0
-		ns.isHealing = (t3 > t1) and (t3 > t2)
-		tankCheck()
+		ns.isTanking = GetPrimaryTalentTree() == 1 and GetShapeshiftForm() == 1
 	end)
-	return
-end
 
-if playerClass == "PALADIN" then
-	TANKFORM = GetSpellInfo(25780)
-	ns.eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
+elseif playerClass == "DRUID" then
+
+	ns.eventFrame:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
+	ns.eventFrame:SetScript("OnEvent", function()
+		ns.isHealing = GetPrimaryTalentTree() == 3
+		ns.isTanking = GetPrimaryTalentTree() == 2 and GetShapeshiftForm() == 1
+	end)
+
+elseif playerClass == "PALADIN" then
+
+	local RighteousFury = GetSpellInfo(25780)
 	ns.eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
 	ns.eventFrame:SetScript("OnEvent", function()
-		local t1, t2, t3 = select(3, GetTalentTabInfo(1)) or 0, select(3, GetTalentTabInfo(2)) or 0, select(3, GetTalentTabInfo(3)) or 0
-		ns.isHealing = (t1 > t2) and (t1 > t3)
-		ns.isTanking = UnitAura("player", TANKFORM, "HELPFUL")
+		ns.isHealing = GetPrimaryTalentTree() == 1
+		ns.isTanking = UnitAura("player", RighteousFury, "HELPFUL")
 	end)
-	return
-end
 
-if playerClass == "PRIEST" then
-	ns.eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
+elseif playerClass == "PRIEST" then
+
 	ns.eventFrame:SetScript("OnEvent", function()
-		local t1, t2, t3 = select(3, GetTalentTabInfo(1)) or 0, select(3, GetTalentTabInfo(2)) or 0, select(3, GetTalentTabInfo(3)) or 0
-		ns.isHealing = (t1 > t3) or (t2 > t3)
+		ns.isHealing = GetPrimaryTalentTree() ~= 3
 	end)
-	return
-end
 
-if playerClass == "SHAMAN" then
-	ns.eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
+elseif playerClass == "SHAMAN" then
+
 	ns.eventFrame:SetScript("OnEvent", function(_, event)
-		local t1, t2, t3 = select(3, GetTalentTabInfo(1)) or 0, select(3, GetTalentTabInfo(2)) or 0, select(3, GetTalentTabInfo(3)) or 0
-		ns.isHealing = (t3 > t1) and (t3 > t2)
+		ns.isHealing = GetPrimaryTalentTree() == 3
 	end)
-	return
-end
 
-if playerClass == "WARRIOR" then
-	TANKFORM = GetSpellInfo(71)
+elseif playerClass == "WARRIOR" then
+
 	ns.eventFrame:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
-	ns.eventFrame:SetScript("OnEvent", tankCheck)
-	return
+	ns.eventFrame:SetScript("OnEvent", function()
+		ns.isTanking = GetPrimaryTalentTree() == 3 and GetShapeshiftForm() == 2
+	end)
+
 end
