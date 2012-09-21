1aac118f31891ade2bee77a7de3c41c3e84a36e4
Phanx-28751 <Phanx-28751@3d716d5e-9e32-4536-a898-9b464fb8abd5>
2012-09-21 07:53:38 +0000

    - Make sure warlock power is hidden when it should be hidden... also when dead or in a vehicle

--- trunk/elements/WarlockPower.lua
+++ trunk/elements/WarlockPower.lua
@@ -37,7 +37,7 @@ local function Update(self, event, unit, powerType)
 		element:PreUpdate()
 	end
 
-	--print("WarlockPower.Update", powerType)
+	print("WarlockPower.Update", powerType)
 	if not powerType then
 		powerType = element.powerType
 		if not powerType then
@@ -51,7 +51,7 @@ local function Update(self, event, unit, powerType)
 	if powerType == "SOUL_SHARDS" then
 		num = UnitPower(unit, SPELL_POWER_SOUL_SHARDS)
 		max = UnitPowerMax(unit, SPELL_POWER_SOUL_SHARDS)
-		--print(powerType, num, "/", max)
+		print(powerType, num, "/", max)
 
 		if element.SetValue then
 			element:SetMinMaxValues(0, max)
@@ -85,7 +85,7 @@ local function Update(self, event, unit, powerType)
 	elseif powerType == "DEMONIC_FURY" then
 		num = UnitPower(unit, SPELL_POWER_DEMONIC_FURY)
 		max = UnitPowerMax(unit, SPELL_POWER_DEMONIC_FURY)
-		--print(powerType, num, "/", max)
+		print(powerType, num, "/", max)
 
 		local activated
 		for i = 1, 40 do
@@ -107,7 +107,7 @@ local function Update(self, event, unit, powerType)
 			for i = 1, #element do
 				local part = element[i]
 				if num > valuePerPart then
-					--print(i, "Full")
+					print(i, "Full")
 					if part.SetValue then
 						part:SetMinMaxValues(0, valuePerPart)
 						part:SetValue(valuePerPart)
@@ -117,20 +117,20 @@ local function Update(self, event, unit, powerType)
 					part:Show()
 					num = num - valuePerPart
 				elseif num > 0 then
-					--print(i, "Partial", num)
+					print(i, "Partial", num)
 					if part.SetValue then
-						--print("part.SetValue")
+						print("part.SetValue")
 						part:SetMinMaxValues(0, valuePerPart)
 						part:SetValue(num)
 					else
-						--print("part.SetAlpha")
+						print("part.SetAlpha")
 						local percent = num / valuePerPart
 						part:SetAlpha(1 - (0.75 * (percent)))
 					end
 					part:Show()
 					num = 0
 				else
-					--print(i, "Empty")
+					print(i, "Empty")
 					if part.SetValue then
 						part:SetMinMaxValues(0, valuePerPart)
 						part:SetValue(0)
@@ -153,20 +153,20 @@ local function Update(self, event, unit, powerType)
 		local part = num % MAX_POWER_PER_EMBER
 
 		if element.SetValue then
-			--print("element.SetValue")
+			print("element.SetValue")
 			element:SetMinMaxValues(0, max)
 			element:SetValue(num)
 		else
-			--print("#elements")
+			print("#elements")
 			for i = 1, #element do
 				local ember = element[i]
 				if i > maxWhole then
-					--print(i, "Hide")
+					print(i, "Hide")
 					ember:Hide()
 				else
-					--print(i, "Show")
+					print(i, "Show")
 					if num > MAX_POWER_PER_EMBER then
-						--print(i, "Full")
+						print(i, "Full")
 						if ember.SetValue then
 							ember:SetMinMaxValues(0, MAX_POWER_PER_EMBER)
 							ember:SetValue(MAX_POWER_PER_EMBER)
@@ -179,7 +179,7 @@ local function Update(self, event, unit, powerType)
 						ember:Show()
 						num = num - MAX_POWER_PER_EMBER
 					elseif num > 0 then
-						--print(i, "Partial", num, part)
+						print(i, "Partial", num, part)
 						if ember.SetValue then
 							ember:SetMinMaxValues(0, MAX_POWER_PER_EMBER)
 							ember:SetValue(num)
@@ -190,7 +190,7 @@ local function Update(self, event, unit, powerType)
 						ember:Show()
 						num = 0
 					else
-						--print(i, "Empty")
+						print(i, "Empty")
 						if ember.SetValue then
 							ember:SetMinMaxValues(0, MAX_POWER_PER_EMBER)
 							ember:SetValue(0)
@@ -209,16 +209,27 @@ local function Update(self, event, unit, powerType)
 	end
 end
 
+local function Path(self, ...)
+	return (self.WarlockPower.Override or Update)(self, ...)
+end
+
+local function ForceUpdate(element)
+	return Path(element.__owner, "ForceUpdate", element.__owner.unit)
+end
+
 local function Visibility(self, event, unit)
 	local spec = GetSpecialization()
-	--print("WarlockPower Visibility", spec)
+	print("WarlockPower: Visibility", spec)
 
 	local element = self.WarlockPower
 	element.powerType = powerTypes[spec]
 
 	local show
-	if spec == SPEC_WARLOCK_AFFLICTION then
-		--print("AFFLICTION")
+	if UnitIsDeadOrGhost("player") or UnitHasVehicleUI("player") then
+		print("dead or in vehicle")
+		-- no show
+	elseif spec == SPEC_WARLOCK_AFFLICTION then
+		print("AFFLICTION")
 		if IsPlayerSpell(WARLOCK_SOULBURN) then
 			show = true
 		else
@@ -226,14 +237,14 @@ local function Visibility(self, event, unit)
 			self:RegisterEvent("SPELLS_CHANGED", Visibility, true)
 		end
 	elseif spec == SPEC_WARLOCK_DEMONOLOGY then
-		--print("DEMONOLOGY")
+		print("DEMONOLOGY")
 		show = true
 		if element.areOrbs then -- oooorrrrbssss!
 			element:SetOrientation("HORIZONTAL")
 			element:SetReverseFill(true)
 		end
 	elseif spec == SPEC_WARLOCK_DESTRUCTION then
-		--print("DESTRUCTION")
+		print("DESTRUCTION")
 		if IsPlayerSpell(WARLOCK_BURNING_EMBERS) then
 			show = true
 			if element.areOrbs then -- oooorrrrbssss!
@@ -247,15 +258,23 @@ local function Visibility(self, event, unit)
 	end
 	self.spec = spec
 
-
 	if show then
-		--print("show")
+		print("show")
 		if element.Show then
 			element:Show()
 		end
+
+		self:RegisterEvent("UNIT_POWER", Path)
+		self:RegisterEvent("UNIT_DISPLAYPOWER", Path)
+
+		self:UnregisterEvent("SPELLS_CHANGED", Visibility)
+
 		element:ForceUpdate("Visibility", "player")
 	else
-		--print("hide")
+		print("hide")
+		self:UnregisterEvent("UNIT_POWER", Path)
+		self:UnregisterEvent("UNIT_DISPLAYPOWER", Path)
+
 		if element.Hide then
 			element:Hide()
 		else
@@ -266,23 +285,18 @@ local function Visibility(self, event, unit)
 	end
 end
 
-local function Path(self, ...)
-	return (self.WarlockPower.Override or Update)(self, ...)
-end
-
-local function ForceUpdate(element)
-	return Path(element.__owner, "ForceUpdate", element.__owner.unit)
-end
-
 local function Enable(self, unit)
 	local element = self.WarlockPower
-	if(element and unit == "player") then
+	if element and unit == "player" then
 		element.__owner = self
 		element.ForceUpdate = ForceUpdate
 
-		self:RegisterEvent("UNIT_POWER", Path)
-		self:RegisterEvent("UNIT_DISPLAYPOWER", Path)
+		self:RegisterEvent("PLAYER_ALIVE", Visibility, true)
+		self:RegisterEvent("PLAYER_DEAD", Visibility, true)
+		self:RegisterEvent("PLAYER_UNGHOST", Visibility, true)
 		self:RegisterEvent("PLAYER_TALENT_UPDATE", Visibility, true)
+		self:RegisterEvent("UNIT_ENTERED_VEHICLE", Visibility)
+		self:RegisterEvent("UNIT_EXITED_VEHICLE", Visibility)
 
 		Visibility(self, nil, "player")
 
@@ -292,10 +306,24 @@ end
 
 local function Disable(self)
 	local element = self.WarlockPower
-	if(element) then
+	if element then
 		self:UnregisterEvent("UNIT_POWER", Path)
 		self:UnregisterEvent("UNIT_DISPLAYPOWER", Path)
+
+		self:UnregisterEvent("PLAYER_ALIVE", Visibility)
+		self:UnregisterEvent("PLAYER_DEAD", Visibility)
+		self:UnregisterEvent("PLAYER_UNGHOST", Visibility)
 		self:UnregisterEvent("PLAYER_TALENT_UPDATE", Visibility)
+		self:UnregisterEvent("UNIT_ENTERED_VEHICLE", Visibility)
+		self:UnregisterEvent("UNIT_EXITED_VEHICLE", Visibility)
+
+		if element.Hide then
+			element:Hide()
+		else
+			for i = 1, #element do
+				element[i]:Hide()
+			end
+		end
 	end
 end
 
