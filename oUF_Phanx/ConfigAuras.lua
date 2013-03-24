daf0454e79b4139562724ea53ba87a6371780e65
Phanx <addons@phanx.net>
2013-03-24 12:06:22 +0000

    - Fix aura options

--- oUF_Phanx/ConfigAuras.lua
+++ oUF_Phanx/ConfigAuras.lua
@@ -17,22 +17,25 @@ L.AuraFilter1 = "Always show"
 L.AuraFilter2 = "Only show mine"
 L.AuraFilter3 = "Only show on friendly units"
 L.AuraFilter4 = "Only show on myself"
+L.DeleteAura = "Delete Aura"
+L.DeleteAura = "Remove your custom filter for this aura."
 L.NewAura = "New Aura"
-L.NewAura_Info = "Add an aura to customize."
+L.NewAura_Info = "Enter a Spell ID and press Enter."
+L.NewAura_Invalid = "That is not a valid Spell ID!"
+L.NewAura_Note = "To find the Spell ID for a spell, look it up on Wowhead.com and copy the number out of the URL."
 
 LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.Auras, "oUF Phanx", function(panel)
 	local title, notes = LibStub("PhanxConfig-Header").CreateHeader(panel, panel.name, L.Auras_Info)
 
-	local add = CreateFrame("Button", "$parentAddButton", panel, "UIPanelButtonTemplate")
-	add:SetText("|TInterface\\LFGFRAME\\LFGROLE_BW:0:0:0:0:64:16:48:64:0:16:255:255:153|t " .. L.NewAura)
-	add:SetPoint("TOPRIGHT", title, "BOTTOMRIGHT", 0, -8)
-	add:SetSize(160, 32)
-	notes:SetPoint("TOPRIGHT", add, "TOPLEFT", -24, 0)
-	panel.addButton = add
-
 	local scrollFrame = CreateFrame("ScrollFrame", "$parentScrollFrame", panel, "UIPanelScrollFrameTemplate")
 	scrollFrame:SetPoint("TOPLEFT", notes, "BOTTOMLEFT", 0, -16)
 	scrollFrame:SetPoint("BOTTOMRIGHT", -38, 16)
+	panel.ScrollFrame = scrollFrame
+
+	scrollFrame.ScrollBar:EnableMouseWheel(true)
+	scrollFrame.ScrollBar:SetScript("OnMouseWheel", function(self, delta)
+		ScrollFrameTemplate_OnMouseWheel(scrollFrame, delta)
+	end)
 
 	local barBG = scrollFrame:CreateTexture(nil, "BACKGROUND", nil, -6)
 	barBG:SetPoint("TOP")
@@ -42,7 +45,7 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.Auras, "oUF Phanx", fun
 	barBG:SetTexture("Interface\\PaperDollInfoFrame\\UI-Character-ScrollBar")
 	barBG:SetTexCoord(0, 0.45, 0.1640625, 1)
 	barBG:SetAlpha(0.5)
-	scrollFrame.barBG = barBG
+	scrollFrame.ScrollBarBG = barBG
 
 	local scrollChild = CreateFrame("Frame", nil, scrollFrame)
 	scrollChild:SetSize(scrollFrame:GetWidth(), 100)
@@ -51,104 +54,240 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.Auras, "oUF Phanx", fun
 	scrollFrame:SetScript("OnSizeChanged", function(self)
 		scrollChild:SetWidth(self:GetWidth())
 	end)
-
+	scrollFrame.ScrollChild = scrollChild
+--[[
 	local scrollBG = scrollChild:CreateTexture(nil, "BACKGROUND")
 	scrollBG:SetAllPoints(true)
-	scrollBG:SetTexture(0, 1, 0, 0.3)
-
+	scrollBG:SetTexture(0, 1, 0, 0.1)
+	scrollChild.BG = scrollBG
+]]
 	--------------------------------------------------------------------
 
 	-- Dialog for adding an aura:
 	local dialog = CreateFrame("Frame", nil, scrollFrame)
-	dialog:SetPoint("BOTTOMLEFT")
-	dialog:SetPoint("TOPRIGHT", barBG)
+	dialog:SetAllPoints(true)
 	dialog:SetBackdrop({
 		bgFile = "Interface\\Tooltips\\UI-Tooltip-Backdrop", tile = true, tileSize = 16,
 		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16,
 		insets = { left = 5, right = 5, top = 5, bottom = 5 },
 	})
-	dialog:SetBackdropColor(0, 0, 0, 1)
+	dialog:SetBackdropColor(0, 0, 0, 0.75)
 	dialog:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
-	panel.dialog = dialog
+	panel.Dialog = dialog
 
-	local dialogTitle, dialogNotes = LibStub("PhanxConfig-Header").CreateHeader(dialog, L.NewAura, L.NewAura_Info)
+	dialog.Title, dialog.Notes = LibStub("PhanxConfig-Header").CreateHeader(dialog, L.NewAura, L.NewAura_Info)
 
-	local editBox = CreateFrame("EditBox", nil, dialog, "UIPanelEditBoxTemplate")
+	local dialogBox = CreateFrame("EditBox", nil, dialog, "InputBoxTemplate")
+	dialogBox:SetPoint("TOPLEFT", dialog.Notes, "BOTTOMLEFT", 10, -24)
+	dialogBox:SetSize(160, 24)
+	dialogBox:SetAltArrowKeyMode(false)
+	dialogBox:SetAutoFocus(false)
+	dialogBox:SetMaxLetters(6)
+	dialogBox:SetScript("OnTextChanged", function(self, userInput)
+		if not userInput then return end
+
+		local text = self:GetText() or ""
+		text = gsub(text, "%D", "")
+		self:SetText(text)
+		self:SetCursorPosition(self:GetNumLetters())
+
+		local spell = self:GetNumber()
+
+		if spell == 0 then
+			dialog.Text:SetText("")
+			dialog.Button:SetEnabled(false)
+			return
+		end
+
+		local name, _, icon = GetSpellInfo(spell)
+		if name and icon then
+			dialog.Text:SetFormattedText("|T%s:0|t %s", icon, name)
+			dialog.Button:SetEnabled(true)
+		else
+			dialog.Text:SetText(RED_FONT_COLOR_CODE .. L.NewAura_Invalid .. "|r")
+			dialog.Button:Disable()
+		end
+	end)
+	dialogBox:SetScript("OnEnterPressed", function(self)
+		if not dialog.Button:IsEnabled() then return end
+
+		local id = tonumber(self:GetText())
+		if id and id > 0 and GetSpellInfo(id) then
+			oUFPhanxAuraConfig[id] = 1
+			dialog:Hide()
+		end
+	end)
+	dialog.EditBox = dialogBox
+
+	local dialogButton = CreateFrame("Button", nil, dialog, "UIPanelButtonTemplate")
+	dialogButton:SetPoint("LEFT", dialogBox, "RIGHT", 12, 0)
+	dialogButton:SetWidth(80)
+	dialogButton:SetText(OKAY)
+	dialogButton:SetScript("OnClick", function()
+		dialogBox:GetScript("OnEnterPressed")(dialogBox)
+	end)
+	dialog.Button = dialogButton
+
+	local dialogText = dialog:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+	dialogText:SetPoint("LEFT", dialogButton, "RIGHT", 12, 0)
+	dialogText:SetPoint("RIGHT", dialog, -16, 0)
+	dialogText:SetJustifyH("LEFT")
+	dialogText:SetJustifyV("TOP")
+	dialogText:SetText("")
+	dialog.Text = dialogText
+
+	local dialogHelp = dialog:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+	dialogHelp:SetPoint("TOPLEFT", dialogBox, "BOTTOMLEFT", -10, -24)
+	dialogHelp:SetPoint("RIGHT", dialog, -16, 0)
+	dialogHelp:SetHeight(32)
+	dialogHelp:SetJustifyH("LEFT")
+	dialogHelp:SetJustifyV("TOP")
+	dialogHelp:SetNonSpaceWrap(true)
+	dialogHelp:SetText(L.NewAura_Note)
+	dialog.HelpText = dialogHelp
 
 	dialog:Hide()
 
+	dialog:SetScript("OnShow", function(self)
+		self.EditBox:SetText("")
+		self.Text:SetText("")
+		self.EditBox:SetFocus()
+	end)
+	dialog:SetScript("OnHide", function(self)
+		self.EditBox:SetText("")
+		self.Text:SetText("")
+		panel.refresh()
+	end)
+
 	--------------------------------------------------------------------
 
-	-- OnClick for delete button:
-	local function DeleteAura(self)
-		local aura = self:GetParent().aura
-		oUFPhanxAuraConfig[aura] = nil
+	local add = CreateFrame("Button", "$parentAddButton", panel, "UIPanelButtonTemplate")
+	add:SetText("|TInterface\\LFGFRAME\\LFGROLE_BW:0:0:0:0:64:16:48:64:0:16:255:255:153|t " .. L.NewAura)
+	add:SetPoint("TOPRIGHT", title, "BOTTOMRIGHT", 0, -8)
+	add:SetSize(160, 32)
+	notes:SetPoint("TOPRIGHT", add, "TOPLEFT", -24, 0)
+	panel.AddButton = add
+	add:SetScript("OnClick", function(self)
+		if dialog:IsShown() then
+			dialog:Hide()
+			self:SetText("|TInterface\\LFGFRAME\\LFGROLE_BW:0:0:0:0:64:16:48:64:0:16:255:255:153|t " .. L.NewAura)
+		else
+			dialog:Show()
+			self:SetText(CANCEL)
+		end
+	end)
+
+	--------------------------------------------------------------------
+
+	local function Row_OnEnter(self)
+		self.highlight:Show()
+		GameTooltip:SetOwner(self, "ANCHOR_NONE")
+		GameTooltip:SetPoint("TOPRIGHT", self, "TOPLEFT")
+		GameTooltip:SetSpellByID(self.id)
+		GameTooltip:Show()
+	end
+	local function Row_OnLeave(self)
+		self.highlight:Hide()
+		GameTooltip:Hide()
+	end
+
+	-- Delete button functions:
+	local function Delete_OnClick(self)
+		if GameTooltip:IsOwned(self) then
+			GameTooltip:Hide()
+		end
+		local id = self:GetParent().id
+		oUFPhanxAuraConfig[id] = nil
 		panel.refresh()
 		ns.UpdateAuraList()
 	end
-
-	-- OnEnter for icon:
-	local function ShowAuraTooltip(self)
-		local aura = self:GetParent().aura
-		GameTooltip:SetAnchor(icon, "ANCHOR_NONE")
-		GameTooltip:SetPoint("TOPRIGHT", icon, "TOPLEFT")
-		GameTooltip:SetSpellByID(aura)
+	local function Delete_OnEnter(self)
+		GameTooltip:SetOwner(self, "ANCHOR_LEFT")
+		GameTooltip:SetText(L.DeleteAura)
+		GameTooltip:AddLine(L.DeleteAura_Info)
 		GameTooltip:Show()
 	end
 
-	-- Dropdown functions:
+	-- Filter dropdown functions:
 	local CURRENT_AURA, CURRENT_DROPDOWN
-	local function SetAuraFilter(info)
+	local function Dropdown_ClickFunc(info)
 		oUFPhanxAuraConfig[CURRENT_AURA] = info.value
-		CURRENT_DROPDOWN:SetValue(info.value, info.text)
-		ns.UpdateAuraLists()
+		CURRENT_DROPDOWN:SetValue(info.value, L["AuraFilter"..info.value])
+		ns.UpdateAuraList()
 	end
-	local function UpdateFilterDrop(self, level) -- self is the dropdown menu
-		CURRENT_AURA = self:GetParent().aura
-		CURRENT_DROPDOWN = self
+	local function Dropdown_Initialize(self, level) -- self is the dropdown menu
+		if not level then return end
+		CURRENT_DROPDOWN = self:GetParent()
+		CURRENT_AURA = CURRENT_DROPDOWN:GetParent().id
+		local selected = oUFPhanxAuraConfig[CURRENT_AURA]
 		local info = UIDropDownMenu_CreateInfo()
 		for i = 0, 4 do
 			info.text = L["AuraFilter"..i]
 			info.value = i
-			info.func = SetAuraFilter
+			info.checked = i == selected
+			info.func = Dropdown_ClickFunc
 			UIDropDownMenu_AddButton(info, level)
 		end
+	end
+	local function Dropdown_OnEnter(self)
+		Row_OnEnter(self:GetParent())
+	end
+	local function Dropdown_OnLeave(self)
+		Row_OnLeave(self:GetParent())
 	end
 
 	-- Rows:
 	local rows = setmetatable({}, { __index = function(t, i)
 		local row = CreateFrame("Frame", nil, scrollChild)
-		row:SetHeight(32)
+		row:SetHeight(40)
 		row:SetPoint("LEFT")
 		row:SetPoint("RIGHT")
 		if i > 1 then
-			row:SetPoint("TOP", rows[i-1], "BOTTOM", 0, -2)
+			row:SetPoint("TOP", rows[i-1], "BOTTOM", 0, -4)
 		else
 			row:SetPoint("TOP")
 		end
 
-		local delete = CreateFrame("Button", nil, row)
-		delete:SetPoint("LEFT")
-		delete:SetSize(24, 24)
-		delete:SetScript("OnClick", DeleteAura)
+		row:EnableMouse(true)
+		row:SetScript("OnEnter", Row_OnEnter)
+		row:SetScript("OnLeave", Row_OnLeave)
+
+		row:EnableMouseWheel(true)
+		row:SetScript("OnMouseWheel", function(self, delta)
+			ScrollFrameTemplate_OnMouseWheel(scrollFrame, delta)
+		end)
+
+		local highlight = row:CreateTexture(nil, "BACKGROUND")
+		highlight:SetAllPoints(true)
+		highlight:SetTexture([[Interface\QuestFrame\UI-QuestLogTitleHighlight]])
+		highlight:SetVertexColor(0.2, 0.4, 0.8, 0.75)
+		highlight:Hide()
+		row.highlight = highlight
+
+		local delete = CreateFrame("Button", nil, row, "UIPanelCloseButton")
+		delete:SetPoint("LEFT", 0, -1)
+		delete:SetSize(32, 32)
+		delete:SetScript("OnClick", Delete_OnClick)
+		delete:SetScript("OnEnter", Delete_OnEnter)
+		delete:SetScript("OnLeave", GameTooltip_Hide)
 		row.delete = delete
 
-		local icon = CreateFrame("Button", nil, row)
-		icon:SetPoint("LEFT", delete, "RIGHT", 0, 8)
+		local icon = row:CreateTexture(nil, "ARTWORK")
+		icon:SetPoint("LEFT", delete, "RIGHT", 12, 0)
 		icon:SetSize(32, 32)
-		icon:EnableMouse(true)
-		icon:SetScript("OnEnter", ShowAuraTooltip)
-		icon:SetScript("OnLeave", GameTooltip_Hide)
+		icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 		row.icon = icon
 
-		local name = row:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-		name:SetPoint("LEFT", icon, "RIGHT", 0, 8)
+		local name = row:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		name:SetPoint("LEFT", icon, "RIGHT", 12, 0)
 		row.name = name
 
-		local filters = LibStub("PhanxConfig-Dropdown").CreateDropdown(row, nil, nil, UpdateFilterDrop)
-		filters:SetPoint("RIGHT")
-		filters:SetWidth(200)
-		row.filters = filters
+		local filter = LibStub("PhanxConfig-Dropdown").CreateDropdown(row, nil, nil, Dropdown_Initialize)
+		filter:SetPoint("RIGHT", 0, 5)
+		filter:SetWidth(200)
+		filter.OnEnter = Dropdown_OnEnter
+		filter.OnLeave = Dropdown_OnLeave
+		row.filter = filter
 
 		t[i] = row
 		return row
@@ -161,37 +300,39 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.Auras, "oUF Phanx", fun
 	local pool = {}
 	local sortedAuras = {}
 	local sortFunc = function(a, b)
-		return a[2] < b[2] or (a[2] == b[2] and a[1] < b[1])
+		return a.name < b.name or (a.name == b.name and a.id < b.id)
 	end
 	panel.refresh = function()
 		for i = #sortedAuras, 1, -1 do
 			pool[i] = tremove(sortedAuras, i)
 		end
 
-		for aura in pairs(oUFPhanxAuraConfig) do
-			local name, _, icon = GetSpellInfo(aura)
+		for id, filter in pairs(oUFPhanxAuraConfig) do
+			local name, _, icon = GetSpellInfo(id)
 			if name and icon then
 				local t = next(pool) or {}
 				pool[t] = nil
 				t.name = name
 				t.icon = icon
-				t.id = aura
+				t.id = id
+				t.filter = filter
 				tinsert(sortedAuras, t)
 			end
 		end
 		table.sort(sortedAuras, sortFunc)
 
 		local height = 0
-		for i, aura in ipairs(sortedAuras) do
+		for i, t in ipairs(sortedAuras) do
 			local row = rows[i]
-			row.aura = aura[1]
-			row.name:SetText(aura[2])
-			row.icon:SetNormalTexture(aura[3])
+			row.id = t.id
+			row.name:SetText(t.name)
+			row.icon:SetTexture(t.icon)
+			row.filter:SetValue(t.filter, L["AuraFilter"..t.filter])
 			row:Show()
-			height = height + row:GetHeight() + 2
+			height = height + 4 + row:GetHeight()
 		end
 		for i = #sortedAuras + 1, #rows do
-			row:Hide()
+			rows[i]:Hide()
 		end
 		scrollChild:SetHeight(height)
 	end
