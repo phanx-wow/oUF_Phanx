5d600bb45aa17481dbd683cf97b6586ed3f42198
Phanx <addons@phanx.net>
2013-03-28 04:55:24 +0000

    - Fix bug where power bars turn gray when options are opened - Add checks to avoid out of bounds errors with statusbar texcoords - Various fixes and cleanup

--- oUF_Phanx/Config-More.lua
+++ oUF_Phanx/Config-More.lua
@@ -12,79 +12,41 @@ local _, playerClass = UnitClass("player")
 local L = ns.L
 
 L.MoreSettings = "More Settings"
-L.MoreSettings_Info = "These settings will not take effect until the next time you reload your UI."
-L.FrameWidth = "Base Width"
-L.FrameWidth_Info = "Set the base frame width. Some frames are proportionally wider or narrower."
-L.FrameHeight = "Base Height"
-L.FrameHeight_Info = "Set the base frame height."
-L.PowerHeight = "Power Bar Height"
-L.PowerHeight_Info = "Set the height of the power bar, as a percent of the total frame height."
-
-local function CreateSlider(parent)
-	local slider = CreateFrame("Slider", "$parentSlider", parent, "OptionsSliderTemplate")
-	slider:SetWidth(250)
-
-	slider.Text = _G[slider:GetName().."Text"]
-
-	slider.Low = _G[slider:GetName().."Low"]
-	slider.Low:SetPoint("TOPLEFT", slider, "BOTTOMLEFT", 0, 0)
-
-	slider.High = _G[slider:GetName().."High"]
-	slider.High:SetPoint("TOPRIGHT", slider, "BOTTOMRIGHT", 0, 0)
-
-	slider.Value = slider:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-	slider.Value:SetPoint("TOP", slider, "BOTTOM", 0, 0)
-
-	return slider
-end
+L.MoreSettings_Desc = "These settings will not take effect until the next time you reload your UI."
+L.FrameWidth = "Base frame width"
+L.FrameWidth_Desc = "Set the base frame width. Some frames are proportionally wider or narrower."
+L.FrameHeight = "Base frame height"
+L.FrameHeight_Desc = "Set the base frame height."
+L.PowerHeight = "Power bar height"
+L.PowerHeight_Desc = "Set the height of the power bar, as a percent of the total frame height."
 
 LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phanx", function(self)
-	local title, notes = LibStub("PhanxConfig-Header").CreateHeader(self, self.name, L.MoreSettings_Info)
+	local CreateSlider = LibStub("PhanxConfig-Slider").CreateSlider
 
-	local FrameWidth = CreateSlider(self)
-	FrameWidth:SetPoint("TOPLEFT", notes, "BOTTOMLEFT", 6, 30)
-	FrameWidth:SetPoint("TORPIGHT", notes, "BOTTOM", -10, 30)
-	FrameWidth.Text:SetText(L.FrameWidth)
-	FrameWidth.tooltipText = L.FrameWidth_Info
-	FrameWidth.Low:SetText(100)
-	FrameWidth.High:SetText(400)
-	FrameWidth:SetMinMaxValues(100, 400)
-	FrameWidth:SetValueStep(20)
-	FrameWidth:SetScript("OnValueChanged", function(this, value)
-		value = floor(value + 0.5)
+	local title, notes = LibStub("PhanxConfig-Header").CreateHeader(self, self.name, L.MoreSettings_Desc)
+
+	--------------------------------------------------------------------
+
+	local FrameWidth = CreateSlider(self, L.FrameWidth, L.FrameWidth_Desc, 100, 400, 20)
+	FrameWidth:SetPoint("TOPLEFT", notes, "BOTTOMLEFT", 0, -12)
+	FrameWidth:SetPoint("TOPRIGHT", notes, "BOTTOM", -12, -12)
+	function FrameWidth:OnValueChanged(value)
 		ns.config.width = value
-		this.Value:SetText(value)
-	end)
+	end
 
-	local FrameHeight = CreateSlider(self)
-	FrameHeight:SetPoint("TOPLEFT", FrameWidth, "BOTTOMLEFT", 0, 30)
-	FrameHeight:SetPoint("TOPRIGHT", FrameWidth, "BOTTOMRIGHT", 0, 30)
-	FrameHeight.Text:SetText(L.FrameHeight)
-	FrameHeight.tooltipText = L.FrameHeight_Info
-	FrameHeight.Low:SetText(100)
-	FrameHeight.High:SetText(400)
-	FrameHeight:SetMinMaxValues(100, 400)
-	FrameHeight:SetValueStep(20)
-	FrameHeight:SetScript("OnValueChanged", function(this, value)
-		value = floor(value + 0.5)
+	local FrameHeight = CreateSlider(self, L.FrameHeight, L.FrameHeight_Desc, 10, 60, 5)
+	FrameHeight:SetPoint("TOPLEFT", FrameWidth, "BOTTOMLEFT", 0, -24)
+	FrameHeight:SetPoint("TOPRIGHT", FrameWidth, "BOTTOMRIGHT", 0, -24)
+	function FrameHeight:OnValueChanged(value)
 		ns.config.height = value
-		this.Value:SetText(value)
-	end)
+	end
 
-	local PowerHeight = CreateSlider(self)
-	PowerHeight:SetPoint("TOPLEFT", FrameHeight, "BOTTOMLEFT", 0, 30)
-	PowerHeight:SetPoint("TOPRIGHT", FrameHeight, "BOTTOMRIGHT", 0, 30)
-	PowerHeight.Text:SetText(L.PowerHeight)
-	PowerHeight.tooltipText = L.PowerHeight_Info
-	PowerHeight.Low:SetText("10%")
-	PowerHeight.High:SetText("50%")
-	PowerHeight:SetMinMaxValues(10, 50)
-	PowerHeight:SetValueStep(5)
-	PowerHeight:SetScript("OnValueChanged", function(this, value)
-		value = floor(value + 0.5)
-		ns.config.powerHeight = value / 100
-		this.Value:SetFormattedText("%d%%", value)
-	end)
+	local PowerHeight = CreateSlider(self, L.PowerHeight, L.PowerHeight_Desc, 0.1, 0.5, 0.05, true)
+	PowerHeight:SetPoint("TOPLEFT", FrameHeight, "BOTTOMLEFT", 0, -24)
+	PowerHeight:SetPoint("TOPRIGHT", FrameHeight, "BOTTOMRIGHT", 0, -24)
+	function PowerHeight:OnValueChanged(value)
+		ns.config.powerHeight = value
+	end
 
 	--------------------------------------------------------------------
 
@@ -92,9 +54,9 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phan
 
 	if playerClass == "DEATHKNIGHT" then
 		RuneBars = CreateFrame("CheckButton", nil, self, "InterfaceOptionsCheckButtonTemplate")
-		RuneBars:SetPoint("TOPLEFT", notes, "BOTTOM", 6, -8)
+		RuneBars:SetPoint("TOPLEFT", notes, "BOTTOM", 12, -24)
 		RuneBars.Text:SetText(L.RuneBars)
-		RuneBars.tooltipText = L.RuneBars_Info
+		RuneBars.tooltipText = L.RuneBars_Desc
 		RuneBars:SetScript("OnClick", function(this)
 			local checked = not not this:GetChecked()
 			PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
@@ -103,9 +65,9 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phan
 
 	elseif playerClass == "DRUID" then
 		DruidMana = CreateFrame("CheckButton", nil, self, "InterfaceOptionsCheckButtonTemplate")
-		DruidMana:SetPoint("TOPLEFT", notes, "BOTTOM", 6, -8)
+		DruidMana:SetPoint("TOPLEFT", notes, "BOTTOM", 12, -24)
 		DruidMana.Text:SetText(L.DruidManaBar)
-		DruidMana.tooltipText = L.DruidManaBar_Info
+		DruidMana.tooltipText = L.DruidManaBar_Desc
 		DruidMana:SetScript("OnClick", function(this)
 			local checked = not not this:GetChecked()
 			PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
@@ -113,9 +75,9 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phan
 		end)
 
 		EclipseBar = CreateFrame("CheckButton", nil, self, "InterfaceOptionsCheckButtonTemplate")
-		EclipseBar:SetPoint("TOPLEFT", DruidMana, "BOTTOMLEFT", 0, -8)
+		EclipseBar:SetPoint("TOPLEFT", DruidMana, "BOTTOMLEFT", 0, -12)
 		EclipseBar.Text:SetText(L.EclipseBar)
-		EclipseBar.tooltipText = L.EclipseBar_Info
+		EclipseBar.tooltipText = L.EclipseBar_Desc
 		EclipseBar:SetScript("OnClick", function(this)
 			local checked = not not this:GetChecked()
 			PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
@@ -124,20 +86,20 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phan
 		end)
 
 		EclipseBarIcons = CreateFrame("CheckButton", nil, self, "InterfaceOptionsCheckButtonTemplate")
-		EclipseBarIcons:SetPoint("TOPLEFT", EclipseBar, "BOTTOMLEFT", 0, -8)
+		EclipseBarIcons:SetPoint("TOPLEFT", EclipseBar, "BOTTOMLEFT", 0, -12)
 		EclipseBarIcons.Text:SetText(L.EclipseBarIcons)
-		EclipseBarIcons.tooltipText = L.EclipseBarIcons_Info
+		EclipseBarIcons.tooltipText = L.EclipseBarIcons_Desc
 		EclipseBarIcons:SetScript("OnClick", function(this)
 			local checked = not not this:GetChecked()
 			PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
 			ns.config.eclipseBarIcons = checked
 		end)
 
-	elseif playerclass == "SHAMAN" then
+	elseif playerClass == "SHAMAN" then
 		TotemBars = CreateFrame("CheckButton", nil, self, "InterfaceOptionsCheckButtonTemplate")
-		TotemBars:SetPoint("TOPLEFT", notes, "BOTTOM", 6, -8)
+		TotemBars:SetPoint("TOPLEFT", notes, "BOTTOM", 12, -24)
 		TotemBars.Text:SetText(L.TotemBars)
-		TotemBars.tooltipText = L.TotemBars_Info
+		TotemBars.tooltipText = L.TotemBars_Desc
 		TotemBars:SetScript("OnClick", function(this)
 			local checked = not not this:GetChecked()
 			PlaySound(checked and "igMainMenuOptionCheckBoxOn" or "igMainmenuOptionCheckBoxOff")
@@ -154,18 +116,22 @@ LibStub("PhanxConfig-OptionsPanel").CreateOptionsPanel(L.MoreSettings, "oUF Phan
 		PowerHeight:SetValue(ns.config.powerHeight)
 
 		if RuneBars then
-			RuneBars:SetChecked(db.runeBars)
+			RuneBars:SetChecked(ns.config.runeBars)
 		end
 
 		if DruidMana then
-			DruidMana:SetChecked(db.druidMana)
-			EclipseBar:SetChecked(db.eclipseBar)
-			EclipseBarIcons:SetChecked(db.eclipseBarIcons)
-			EclipseBarIcons:SetEnabled(db.eclipseBar)
+			DruidMana:SetChecked(ns.config.druidMana)
+			EclipseBar:SetChecked(ns.config.eclipseBar)
+			EclipseBarIcons:SetChecked(ns.config.eclipseBarIcons)
+			EclipseBarIcons:SetEnabled(ns.config.eclipseBar)
 		end
 
 		if TotemBars then
-			TotemBars:SetChecked(db.totemBars)
+			TotemBars:SetChecked(ns.config.totemBars)
+		end
+
+		for i = 1, #oUF.objects do
+			oUF.objects[i]:UpdateAllElements("OptionsRefresh")
 		end
 	end
 end)
