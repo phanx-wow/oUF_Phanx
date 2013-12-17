9e7542a89ce582605b2b24f29de41a6714a5a2ce
Phanx-28751 <Phanx-28751@3d716d5e-9e32-4536-a898-9b464fb8abd5>
2013-12-17 10:34:39 +0000

    - Adjust some German translations for grammar and length

--- trunk/oUF_Phanx/Locales/Locale-de.lua
+++ trunk/oUF_Phanx/Locales/Locale-de.lua
@@ -10,7 +10,7 @@
 	Contributors: Grafotz
 ----------------------------------------------------------------------]]
 
-if GetLocale() ~= "deDE" then return end
+--if GetLocale() ~= "deDE" then return end
 local _, private = ...
 local L = private.L
 
@@ -44,8 +44,8 @@ L.EclipseBar = "Finsternisbalken"
 L.EclipseBar_Desc = "Ein Finsternisbalken über dem Spielerfenster anzeigen."
 L.EclipseBarIcons = "Finsternisbalkensymbole"
 L.EclipseBarIcons_Desc = "Animierte Symbole von Mond und Sonne an beiden Enden der Finsternisbalken anzeigen."
-L.FilterDebuffHighlight = "Hervorhebungen der Schwächungszauber filtern"
-L.FilterDebuffHighlight_Desc = "Nur Hervorhebungen der Schwächungszauber anzeigen, die auch entfernt werden können."
+L.FilterDebuffHighlight = "Schwächungszauber filtern"
+L.FilterDebuffHighlight_Desc = "Hervorhebungen der Schwächungszauber nur anzeigen, die auch entfernt werden können."
 L.Font = "Schriftart"
 L.FrameHeight = "Basishöhe"
 L.FrameHeight_Desc = "Die Basishöhe der Fenster festlegen."
@@ -55,11 +55,11 @@ L.HealthBG = "Gesundheitsbalkenhintergrund"
 L.HealthBG_Desc = "Die Helligkeit der Hintergrund des Gesundheitsbalkens festlegen, relativ zu seiner Vordergrund."
 L.HealthColor = "Gesundheitsbalkenfarbe"
 L.HealthColor_Desc = "Legt fest, wie die Gesundheitsbalken eingefärbt werden."
-L.HealthColorCustom = "Benutzerdefinierte Gesundheitsfarbe"
+L.HealthColorCustom = "Benutzerdefinierte Farbe"
 L.IgnoreOwnHeals = "Ignoriere eigene Heals"
 L.IgnoreOwnHeals_Desc = "Zeige nur eingehende Heilungen anderer Spieler."
 L.MoreSettings = "Weitere Einstellungen"
-L.MoreSettings_Desc = "Um diese Einstellungen zu ändern, muß das UI erneut geladen werden."
+L.MoreSettings_Desc = "Um die Änderungen dieser Einstellungen anzuwenden, muss das UI erneut geladen werden."
 L.None = "Nichts"
 L.Options_Desc = "oUF_Phanx ist ein Layout für Haste's oUF Framework. Nutze diese Oberfläche um Grundeinstellungen zu konfigurieren."
 L.Outline = "Schriftumriss"
@@ -67,7 +67,7 @@ L.PowerBG = "Ressourcenbalkenhintergrund"
 L.PowerBG_Desc = "Die Helligkeit der Hintergrund des Ressourcenbalkens festlegen, relativ zu seiner Vordergrund."
 L.PowerColor = "Ressourcenbalkenfarbe"
 L.PowerColor_Desc = "Legt fest, wie die Ressourcenbalken eingefärbt werden."
-L.PowerColorCustom = "Benutzerdefinierte Ressourcenfarbe"
+L.PowerColorCustom = "Benutzerdefinierte Farbe"
 L.PowerHeight = "Ressourcenbalkenhöhe"
 L.PowerHeight_Desc = "Die Höhe des Ressourcenbalkens festlegen, als ein Prozent der Gesamthöhe des Fensters."
 L.ReloadUI = "UI neuladen"
