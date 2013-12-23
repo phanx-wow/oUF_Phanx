623ca48dec91eadf99c399444e2e1e69b7409f10
Phanx <addons@phanx.net>
2013-12-23 07:50:01 +0000

    - Add 2 zhTW translations from zhTW on CurseForge (honestly, though, why bother?!)

---
+++ oUF_Phanx/Locales/Locale-zhTW.lua
@@ -0,0 +1,82 @@
+--[[--------------------------------------------------------------------
+	oUF_Phanx
+	Fully-featured PVE-oriented layout for oUF.
+	Copyright (c) 2008-2013 Phanx <addons@phanx.net>. All rights reserved.
+	See the accompanying README and LICENSE files for more information.
+	http://www.wowinterface.com/downloads/info13993-oUF_Phanx.html
+	http://www.curse.com/addons/wow/ouf-phanx
+------------------------------------------------------------------------
+	Traditional Chinese localization
+	Contributors: zhTW
+----------------------------------------------------------------------]]
+
+if GetLocale() ~= "zhTW" then return end
+local _, private = ...
+local L = private.L
+
+--L.AddAura = ""
+--L.AddAura_Desc = ""
+--L.AddAura_Invalid = ""
+--L.AddAura_Note = ""
+--L.Auras = ""
+--L.Auras_Desc = ""
+--L.AuraFilter0 = ""
+--L.AuraFilter1 = ""
+--L.AuraFilter2 = ""
+--L.AuraFilter3 = ""
+--L.AuraFilter4 = ""
+--L.BorderColor = ""
+--L.BorderColor_Desc = ""
+--L.BorderSize = ""
+--L.ColorClass = ""
+L.ColorCustom = "使用自定義顏色"
+--L.ColorHealth = ""
+--L.ColorPower = ""
+--L.Colors = ""
+--L.Colors_Desc = ""
+--L.CombatText = ""
+--L.CombatText_Desc = ""
+--L.DeleteAura = ""
+--L.DeleteAura_Desc = ""
+--L.DruidManaBar = ""
+--L.DruidManaBar_Desc = ""
+--L.EclipseBar = ""
+--L.EclipseBar_Desc = ""
+--L.EclipseBarIcons = ""
+--L.EclipseBarIcons_Desc = ""
+--L.FilterDebuffHighlight = ""
+--L.FilterDebuffHighlight_Desc = ""
+--L.Font = ""
+--L.FrameHeight = ""
+--L.FrameHeight_Desc = ""
+--L.FrameWidth = ""
+--L.FrameWidth_Desc = ""
+--L.HealthBG = ""
+--L.HealthBG_Desc = ""
+--L.HealthColor = ""
+--L.HealthColor_Desc = ""
+--L.HealthColorCustom = ""
+--L.IgnoreOwnHeals = ""
+--L.IgnoreOwnHeals_Desc = ""
+--L.MoreSettings = ""
+--L.MoreSettings_Desc = ""
+--L.None = ""
+--L.Options_Desc = ""
+--L.Outline = ""
+--L.PowerBG = ""
+--L.PowerBG_Desc = ""
+--L.PowerColor = ""
+--L.PowerColor_Desc = ""
+--L.PowerColorCustom = ""
+--L.PowerHeight = ""
+--L.PowerHeight_Desc = ""
+L.ReloadUI = "重載UI"
+--L.RuneBars = ""
+--L.RuneBars_Desc = ""
+--L.Texture = ""
+--L.Thick = ""
+--L.Thin = ""
+--L.ThreatLevels = ""
+--L.ThreatLevels_Desc = ""
+--L.TotemBars = ""
+--L.TotemBars_Desc = ""
