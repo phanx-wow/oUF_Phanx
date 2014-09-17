e657060853c5cbc43585b5e8fdbc4949727c64b5
Phanx <addons@phanx.net>
2014-09-17 15:42:31 +0000

    - Greatly simplify role checking

--- oUF_Phanx/Role.lua
+++ oUF_Phanx/Role.lua
@@ -8,102 +8,35 @@
 ----------------------------------------------------------------------]]
 
 local _, ns = ...
-local playerClass = select(2, UnitClass("player"))
+local _, playerClass = UnitClass("player")
 
 local CURRENT_ROLE = "DAMAGER"
-local getRole, updateEvents
 
 function ns.GetPlayerRole()
 	return CURRENT_ROLE
 end
 
-if playerClass == "DEATHKNIGHT" then
-	updateEvents = "UPDATE_SHAPESHIFT_FORM"
-	function getRole()
-		if GetSpecialization() == 1 then -- Blood 1, Frost 2, Unholy 3
-			return "TANK"
-		end
-	end
-
-elseif playerClass == "DRUID" then
-	updateEvents = "UPDATE_SHAPESHIFT_FORM"
-	function getRole()
-		local form = GetShapeshiftFormID() -- Aquatic 4, Bear 5, Cat 1, Flight 29, Moonkin 31, Swift Flight 27, Travel 3, Tree 2
-		if form == 5 then
-			return "TANK"
-		elseif GetSpecialization() == 4 then -- Balance 1, Feral 2, Guardian 3, Restoration 4
-			return "HEALER"
-		end
-	end
-
-elseif playerClass == "MONK" then
-	updateEvents = "UPDATE_SHAPESHIFT_FORM"
-	function getRole()
-		local form = GetShapeshiftFormID() -- Tiger 24, Ox 23, Serpent 20
-		if form == 23 then
-			return "TANK"
-		elseif form == 20 then
-			return "HEALER"
-		end
-	end
-
-elseif playerClass == "PALADIN" then
-	local RIGHTEOUS_FURY = GetSpellInfo(25780)
-	updateEvents = "PLAYER_REGEN_DISABLED"
-	function getRole()
-		if UnitAura("player", RIGHTEOUS_FURY, "HELPFUL") then
-			return "TANK"
-		elseif GetSpecialization() == 1 then -- Holy 1, Protection 2, Retribution 3
-			return "HEALER"
-		end
-	end
-
-elseif playerClass == "PRIEST" then
-	function getRole()
-		if GetSpecialization() ~= 3 then -- Discipline 1, Holy 2, Shadow 3
-			return "HEALER"
-		end
-	end
-
-elseif playerClass == "SHAMAN" then
-	function getRole()
-		if GetSpecialization() == 3 then -- Elemental 1, Enhancement 2, Restoration 3
-			return "HEALER"
-		end
-	end
-
-elseif playerClass == "WARRIOR" then
-	updateEvents = "UPDATE_SHAPESHIFT_FORM"
-	function getRole()
-		if GetSpecialization() == 3 and GetShapeshiftFormID() == 18 then -- Battle 17, Berserker 19, Defensive 18
-			return "TANK"
-		end
-	end
-
+if playerClass == "HUNTER" or playerClass == "MAGE" or playerClass == "ROGUE" or playerClass == "WARLOCK" then
+	return -- These classes can only be DAMAGER, no need to listen for spec changes.
 end
 
-if getRole then
-	local eventFrame = CreateFrame("Frame")
-	eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-	eventFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
-	if updateEvents then
-		for event in gmatch(updateEvents, "%S+") do
-			eventFrame:RegisterEvent(event)
-		end
-	end
-	eventFrame:SetScript("OnEvent", function(_, event, ...)
-		local role = getRole() or "DAMAGER"
-		if role ~= CURRENT_ROLE then
-			--print(event, CURRENT_ROLE, "->", role)
-			CURRENT_ROLE = role
-			ns.UpdateAuraList()
-			for _, frame in pairs(ns.objects) do
-				if frame.updateOnRoleChange then
-					for _, func in pairs(frame.updateOnRoleChange) do
-						func(frame, role)
-					end
-				end
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+f:SetScript("OnEvent", function(_, event, ...)
+	local spec = GetSpecialization()
+	local role = spec and spec > 0 and select(6, GetSpecializationInfo(spec)) or "DAMAGER"
+	if role == CURRENT_ROLE then return end
+
+	--print(event, CURRENT_ROLE, "->", role)
+	CURRENT_ROLE = role
+	ns.UpdateAuraList()
+
+	for _, frame in pairs(ns.objects) do
+		if frame.updateOnRoleChange then
+			for _, func in pairs(frame.updateOnRoleChange) do
+				func(frame, role)
 			end
 		end
-	end)
-end
+	end
+end)
