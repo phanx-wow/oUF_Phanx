1ca77f52dcda6af0770c31969d220c4e03f3681a
Phanx <addons@phanx.net>
2014-12-11 13:45:19 -0800

    Don't need to manually round slider values
    Also update widget callback names

--- Config/Main.lua
+++ Config/Main.lua
@@ -169,19 +169,18 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 	local shadow = panel:CreateCheckbox(L.Shadow)
 	shadow:SetPoint("TOPLEFT", outline, "BOTTOMLEFT", 0, -12)
 
-	function shadow:Callback(value)
+	function shadow:OnValueChanged(value)
 		db.fontShadow = value
 		ns.SetAllFonts()
 	end
 
 	--------------------------------------------------------------------
 
-	local borderSize = panel:CreateSlider(L.BorderSize, nil, 12, 24, 2)
+	local borderSize = panel:OnValueChanged(L.BorderSize, nil, 12, 24, 2)
 	borderSize:SetPoint("TOPLEFT", shadow, "BOTTOMLEFT", 0, -12)
 	borderSize:SetPoint("TOPRIGHT", outline, "BOTTOMRIGHT", 0, -24 - shadow:GetHeight())
 
-	function borderSize:Callback(value)
-		value = floor(value + 0.5)
+	function borderSize:OnValueChanged(value)
 		db.borderSize = value
 		for i = 1, #ns.borderedObjects do
 			ns.borderedObjects[i]:SetBorderSize(value)
@@ -198,7 +197,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 		return unpack(db.borderColor)
 	end
 
-	function borderColor:Callback(r, g, b)
+	function borderColor:OnValueChanged(r, g, b)
 		db.borderColor[1] = r
 		db.borderColor[2] = g
 		db.borderColor[3] = b
@@ -218,7 +217,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 	local dispelFilter = panel:CreateCheckbox(L.FilterDebuffHighlight, L.FilterDebuffHighlight_Desc)
 	dispelFilter:SetPoint("TOPLEFT", notes, "BOTTOM", 12, -24)
 
-	function dispelFilter:Callback(value)
+	function dispelFilter:OnValueChanged(value)
 		db.dispelFilter = value
 		for i = 1, #ns.objects do
 			local frame = ns.objects[i]
@@ -234,7 +233,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 	local healFilter = panel:CreateCheckbox(L.IgnoreOwnHeals, L.IgnoreOwnHeals_Desc)
 	healFilter:SetPoint("TOPLEFT", dispelFilter, "BOTTOMLEFT", 0, -12)
 
-	function healFilter:Callback(value)
+	function healFilter:OnValueChanged(value)
 		db.ignoreOwnHeals = value
 		for i = 1, #ns.objects do
 			local frame = ns.objects[i]
@@ -249,7 +248,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 	local threatLevels = panel:CreateCheckbox(L.ThreatLevels, L.ThreatLevels_Desc)
 	threatLevels:SetPoint("TOPLEFT", healFilter, "BOTTOMLEFT", 0, -12)
 
-	function threatLevels:Callback(value)
+	function threatLevels:OnValueChanged(value)
 		db.ignoreOwnHeals = value
 		for i = 1, #ns.objects do
 			local frame = ns.objects[i]
@@ -308,7 +307,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 		return unpack(db.healthColor)
 	end
 
-	function healthColor:Callback(r, g, b)
+	function healthColor:OnValueChanged(r, g, b)
 		db.healthColor[1] = r
 		db.healthColor[2] = g
 		db.healthColor[3] = b
@@ -324,12 +323,11 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 
 	--------------------------------------------------------------------
 
-	local healthBG = panel:CreateSlider(L.HealthBG, L.HealthBG_Desc, 0, 3, 0.05, true)
+	local healthBG = panel:OnValueChanged(L.HealthBG, L.HealthBG_Desc, 0, 3, 0.05, true)
 	healthBG:SetPoint("TOPLEFT", healthColorMode, "BOTTOMLEFT", 0, -12)
 	healthBG:SetPoint("TOPRIGHT", healthColorMode, "BOTTOMRIGHT", 0, -12)
 
-	function healthBG:Callback(value)
-		value = math.floor(value * 100 + 0.5) / 100
+	function healthBG:OnValueChanged(value)
 		db.healthBG = value
 		local custom = db.healthColorMode == "CUSTOM"
 		for i = 1, #ns.objects do
@@ -398,7 +396,7 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 		return unpack(db.powerColor)
 	end
 
-	function powerColor:Callback(r, g, b)
+	function powerColor:OnValueChanged(r, g, b)
 		db.powerColor[1] = r
 		db.powerColor[2] = g
 		db.powerColor[3] = b
@@ -415,12 +413,11 @@ LibStub("PhanxConfig-OptionsPanel"):New(oUFPhanxOptions, nil, function(panel)
 
 	--------------------------------------------------------------------
 
-	local powerBG = panel:CreateSlider(L.PowerBG, L.PowerBG_Desc, 0, 3, 0.05, true)
+	local powerBG = panel:OnValueChanged(L.PowerBG, L.PowerBG_Desc, 0, 3, 0.05, true)
 	powerBG:SetPoint("TOPLEFT", powerColorMode, "BOTTOMLEFT", 0, -12)
 	powerBG:SetPoint("TOPRIGHT", powerColorMode, "BOTTOMRIGHT", 0, -12)
 
-	function powerBG:Callback(value)
-		value = floor(value * 100 + 0.5) / 100
+	function powerBG:OnValueChanged(value)
 		db.powerBG = value
 		local custom = db.powerColorMode == "CUSTOM"
 		for i = 1, #ns.objects do
