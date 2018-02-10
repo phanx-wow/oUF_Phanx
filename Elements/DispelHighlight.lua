78c5f92f3dd8215f93eedd356920bbe610d695fd
Phanx <addons@phanx.net>
2018-02-10 03:34:47 -0800

    Remove enrage highlighting
    All enrage dispelling abilities were removed in Legion

--- Elements/DispelHighlight.lua
+++ Elements/DispelHighlight.lua
@@ -35,7 +35,6 @@ local _, playerClass = UnitClass("player")
 local colors = { -- these are nicer than DebuffTypeColor
 	Curse        = { 0.8, 0,   1   },
 	Disease      = { 0.8, 0.6, 0   },
-	Enrage       = { 1,   0.2, 0.6 },
 	Invulnerable = { 1,   1,   0.4 },
 	Magic        = { 0,   0.8, 1   },
 	Poison       = { 0,   0.8, 0   },
@@ -46,10 +45,6 @@ end
 
 -- IDs pulled from wowdb.com using Adirelle's script:
 -- https://github.com/Adirelle/LibDispellable-1.0/blob/master/fetchEnrageList.sh
-local EnrageEffects = {
-	-- curl -s http://www.wowdb.com/spells?filter-dispel-type=9 | perl -ne 'm@http://www.wowdb.com/spells/(\d+)\-@ and print "$1\n";' | sort -nu
-	[8599] = true, [12880] = true, [15061] = true, [15716] = true, [18499] = true, [18501] = true, [19451] = true, [19812] = true, [22428] = true, [23128] = true, [23257] = true, [23342] = true, [24689] = true, [26041] = true, [26051] = true, [28371] = true, [30485] = true, [31540] = true, [31915] = true, [32714] = true, [33958] = true, [34670] = true, [37605] = true, [37648] = true, [37975] = true, [38046] = true, [38166] = true, [38664] = true, [39031] = true, [39575] = true, [40076] = true, [41254] = true, [41447] = true, [42705] = true, [42745] = true, [43139] = true, [47399] = true, [48138] = true, [48142] = true, [48193] = true, [50420] = true, [51513] = true, [52262] = true, [52470] = true, [54427] = true, [55285] = true, [56646] = true, [57733] = true, [58942] = true, [59465] = true, [59697] = true, [59707] = true, [59828] = true, [60075] = true, [61369] = true, [63227] = true, [66092] = true, [68541] = true, [70371] = true, [72143] = true, [75998] = true, [76100] = true, [76862] = true, [77238] = true, [78722] = true, [78943] = true, [80084] = true, [80467] = true, [86736] = true, [102134] = true, [102989] = true, [106925] = true, [108169] = true, [109889] = true, [111220] = true, [115430] = true, [117837] = true, [119629] = true, [123936] = true, [124019] = true, [124309] = true, [126370] = true, [127823] = true, [127955] = true, [128231] = true, [129016] = true, [129874] = true, [130196] = true, [130202] = true, [131150] = true, [135524] = true, [135548] = true, [142760] = true, [148295] = true, [151553] = true, [154017] = true, [155620] = true, [164324] = true, [164835] = true, [175743] = true,
-}
 local InvulnerableEffects = {
 	-- curl -s http://www.wowdb.com/spells?filter-mechanic=25 | perl -ne 'm@http://www.wowdb.com/spells/(\d+)\-@ and print "$1\n";' | sort -nu
 	[25771] = true, [34518] = true, [38916] = true, [41625] = true, [84958] = true,
@@ -92,7 +87,7 @@ function Update(self, event, unit)
 			for i = 1, 40 do
 				local name, _, _, _, type, _, _, _, stealable, _, id = UnitBuff(unit, i)
 				if not name then break end
-				if (canSteal and stealable) or (type and canPurge[EnrageEffects[id] and "Enrage" or InvulnerableEffects[id] and "Invulnerable" or type]) then
+				if (canSteal and stealable) or (type and canPurge[InvulnerableEffects[id] and "Invulnerable" or type]) then
 					-- print("debuffType", type)
 					debuffType = type
 					dispellable = true
@@ -179,11 +174,9 @@ f:SetScript("OnEvent", function(self, event)
 		canDispel.Curse   = IsSpellKnown(88423) or IsSpellKnown(2782) or nil -- Nature's Cure or Remove Corruption
 		canDispel.Magic   = IsSpellKnown(88423) or nil -- Nature's Cure
 		canDispel.Poison  = canDispel.Curse
-		canPurge.Enrage   = IsSpellKnown(2908) or nil -- Soothe
 
 	elseif playerClass == "HUNTER" then
 		canPurge.Magic    = IsSpellKnown(19801) or nil -- Tranquilizing Shot
-		canPurge.Enrage   = canPurge.Magic
 
 	elseif playerClass == "MAGE" then
 		canDispel.Curse   = IsSpellKnown(475)   or nil -- Remove Curse
@@ -203,9 +196,6 @@ f:SetScript("OnEvent", function(self, event)
 		canDispel.Disease = IsSpellKnown(527) -- Purify
 		canDispel.Magic   = IsSpellKnown(527) or IsSpellKnown(32375) or nil -- Purify or Mass Dispel
 		canPurge.Magic    = IsSpellKnown(528) -- Dispel Magic
-
-	elseif playerClass == "ROGUE" then
-		canPurge.Enrage   = IsSpellKnown(5938) -- Shiv
 
 	elseif playerClass == "SHAMAN" then		
 		canDispel.Curse   = IsSpellKnown(77130) or IsSpellKnown(51886) or nil -- Cleanse Spirit or Purify Spirit)
